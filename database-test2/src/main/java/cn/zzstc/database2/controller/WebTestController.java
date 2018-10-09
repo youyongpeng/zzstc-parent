014927f3304b8407c4a7a3ada7181221fc941a56
youyongpeng <youyongpeng0613@qq.com>
2018-10-09 14:12:16 +0800

    update

---
+++ database-test2/src/main/java/cn/zzstc/database2/controller/WebTestController.java
@@ -0,0 +1,12 @@
+package cn.zzstc.database2.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName WebTestController
+ * @Author youyp
+ * @DATE 2018/10/8/008
+ */
+@RestController
+public class WebTestController {
+}
